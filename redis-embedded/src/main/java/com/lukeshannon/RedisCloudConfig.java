29c41a3c9b2da9de1546d3009a5917f0df39df76
lshannon <luke.shannon@gmail.com>
2018-02-21 09:42:43 -0800

    Updated configuration based on some feedback from Mark Paluch

--- redis-embedded/src/main/java/com/lukeshannon/RedisCloudConfig.java
+++ redis-embedded/src/main/java/com/lukeshannon/RedisCloudConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 @Profile("cloud")
--- redis-embedded/src/main/java/com/lukeshannon/RedisTestConfiguration.java
+++ redis-embedded/src/main/java/com/lukeshannon/RedisTestConfiguration.java
@@ -2,8 +2,8 @@ package com.lukeshannon;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -11,14 +11,14 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 /*
  * https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-testing.html#boot-features-testing-spring-boot-applications-detecting-config
  */
-@TestConfiguration
+@Configuration
 @Profile("test")
 public class RedisTestConfiguration {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(RedisTestConfiguration.class);
-	
+
 	@Bean
-	RedisConnectionFactory connectionFactory(EmbeddedRedis embeddedRedisServer) {
+	RedisConnectionFactory redisConnectionFactory(EmbeddedRedis embeddedRedisServer) {
 		log.info("Creating a Redis Connection Factory to embedded Redis");
 		return new LettuceConnectionFactory("127.0.0.1", embeddedRedisServer.getRedisPort());
 	}
